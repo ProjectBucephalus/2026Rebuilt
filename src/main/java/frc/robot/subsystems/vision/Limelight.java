3f1ff5d8d9b45ae0a7ee931fe69b32f045c3dc10
ThePixelatedCat <acfrostie@gmail.com>
2026-01-17 16:09:22 +1100

    More reviewing

--- src/main/java/frc/robot/subsystems/vision/Limelight.java
+++ src/main/java/frc/robot/subsystems/vision/Limelight.java
@@ -23,7 +23,9 @@ import edu.wpi.first.math.geometry.Translation3d;
 public class Limelight  
 {    
   private final PhotonCamera camera;
-  private static final AprilTagFieldLayout kTagLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+  // TODO should probably initialise photonEstimator in the constructor so that the tag layout and robotToCam transform can be just restricted to the constructor
+  // Also, maybe let the robotToCam transform be provided in the constructor
+  private static final AprilTagFieldLayout kTagLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField); 
   private static final Transform3d kRobotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
   private final PhotonPoseEstimator photonEstimator = new PhotonPoseEstimator(kTagLayout, kRobotToCam);
   private PhotonPipelineResult result;
