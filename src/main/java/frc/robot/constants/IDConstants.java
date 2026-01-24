3f0c7521de362c9d89bec48cfa20b733f05b0e32
ThePixelatedCat <acfrostie@gmail.com>
2026-01-24 16:25:34 +1100

    Fixed hood ID

--- src/main/java/frc/robot/constants/IDConstants.java
+++ src/main/java/frc/robot/constants/IDConstants.java
@@ -33,7 +33,7 @@ public final class IDConstants
   public static final int shooterMainID = 22;
   public static final int shooterAuxID = 23;
   public static final int turretID = 21;
-  public static final int hoodID = 20;
+  public static final int hoodID = 1;
   public static final int indexerID = 24;
   public static final int beltID = 25;
   public static final int intakeID = 26;
