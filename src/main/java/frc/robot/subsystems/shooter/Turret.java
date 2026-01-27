10af29f7667c13fcc4323805ddbeb443b1df856e
Lego_archer_elf <Lego.archer.elf@gmail.com>
2026-01-27 17:05:22 +1100

    Calibrating azimuth using potentiometer and handling values as degrees

--- src/main/java/frc/robot/subsystems/shooter/Turret.java
+++ src/main/java/frc/robot/subsystems/shooter/Turret.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.shooter;
 
+import frc.robot.constants.Constants;
 import frc.robot.util.Conversions;
 import frc.robot.util.FieldUtils;
 
@@ -13,21 +14,26 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.units.AngleUnit;
 import edu.wpi.first.units.Units;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 
 public class Turret
 {
   private final TalonFX m_Turret;
+  private final AnalogPotentiometer io_Azimuth;
 
   private final MotionMagicVoltage request = new MotionMagicVoltage(0);
 
   private final Translation2d robotOffset;
 
-  public Turret(int id, Translation2d robotOffset) 
+  public Turret(int motorID, int potID, double potOffset, Translation2d robotOffset) 
   {
     this.robotOffset = robotOffset;
-    m_Turret = new TalonFX(id);
+    m_Turret = new TalonFX(motorID);
+    io_Azimuth = new AnalogPotentiometer(potID, Constants.TurretConstants.maxTurretAzimuth, potOffset);
 
     m_Turret.getConfigurator().apply(turretConfigs);
+
+    calibrate();
   }
 
   /**
@@ -43,24 +49,27 @@ public class Turret
     // Robot-Relative angle from turret to target
     double robotTarget = fieldTarget - robotPose.getRotation().getDegrees();
 
-    return Conversions.normaliseAngle(robotTarget, getRotation().getDegrees(), maxTurretAzimuth);
+    return Conversions.normaliseAngle(robotTarget, getRotation(), maxTurretAzimuth);
   }
 
   public void unwind()
   {
-    double angle = getRotation().getDegrees();
+    double angle = getRotation();
     m_Turret.setControl(request.withPosition(Conversions.normaliseAngle(angle + 360, angle, maxTurretAzimuth) / 360)); 
   }
 
   public void home()
     {m_Turret.setControl(request.withPosition(0));}
 
-  public Rotation2d getRotation() 
-    {return new Rotation2d(m_Turret.getPosition().getValue());}
+  /** Returns mechanism rotation in degrees */
+  public double getRotation() 
+    {return m_Turret.getPosition().getValue().in(Units.Degrees);} // TODO Define and standadise rotation direction
   
   //using the states defined in Target.java to set the place that the turret is tracking
   public void update(Pose2d robotPose, Target target)
   {
+    calibrate();
+    
     double targetAzimuth = switch (target.state) 
     {
       case Manual -> target.azimuth;
@@ -73,4 +82,13 @@ public class Turret
     // gives control of the motors to request to be called in Robot.java
     m_Turret.setControl(request.withPosition(targetAzimuth / 360));     
   }   
+
+  public void calibrate()
+  {
+    // If the turret is not moving, pull the value from the pot, convert to mechanism angle, and send to motor
+    if (Math.abs(m_Turret.getVelocity().getValueAsDouble()) < 0.1)
+    {
+      m_Turret.setPosition((io_Azimuth.get()*gearRatio)/360.0);
+    }
+  }
 }
