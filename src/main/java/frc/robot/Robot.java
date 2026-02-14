7739bc16208bea548f897d5a7415cde2d2ada2b5
Lego_archer_elf <Lego.archer.elf@gmail.com>
2026-02-14 21:30:19 +1100

    Preventing crash

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -208,7 +208,7 @@ public class Robot extends TimedRobot
         driver::getRightTriggerAxis
       )
     );
-
+/* 
     s_PortShooter.shootReadyTrigger()
       .and(s_StbdShooter.shootReadyTrigger())
       .whileTrue
@@ -218,7 +218,7 @@ public class Robot extends TimedRobot
           () -> s_Feeder.setSpeed(Math.min(s_StbdShooter.getSpeed(), s_PortShooter.getSpeed())),
           () -> s_Feeder.setSpeed(0)
         )
-      );
+      );*/
 
     bumpNB.asTrigger()
       .or(bumpSB.asTrigger())
