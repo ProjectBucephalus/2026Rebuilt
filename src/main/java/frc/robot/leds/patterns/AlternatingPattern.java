31803dd1d2fe92dbb7424a21bb47b80333176ba5
Lego-archer-elf <Lego.archer.elf@gmail.com>
2026-04-22 02:50:04 +1000

    Correcting variable naming to match use

--- src/main/java/frc/robot/leds/patterns/AlternatingPattern.java
+++ src/main/java/frc/robot/leds/patterns/AlternatingPattern.java
@@ -10,22 +10,28 @@ public class AlternatingPattern implements LEDPattern
 {
   private final Color colour1;
   private final Color colour2;
-  private final double frequency;
+  private final double period;
 
   private double lastSwap = 0;
-  private boolean evenCycle = false;
+  private boolean evenCycle = true;
 
+  /**
+   * Creates a pattern that sets alternate LEDs to the given colours, swapping at the given frequency
+   * @param colour1 Colour of Even LEDs on first cycle
+   * @param colour2 Colour of Odd LEDs on first cycle
+   * @param frequency Cycles per second, Hz
+   */
   public AlternatingPattern(Color colour1, Color colour2, double frequency)
   {
     this.colour1 = colour1;
     this.colour2 = colour2;
-    this.frequency = frequency;
+    this.period  = 1.0 / frequency;
   }
 
   @Override
   public void applyTo(LEDReader reader, LEDWriter writer) 
   {
-    if (Timer.getTimestamp() - lastSwap >= frequency)
+    if (Timer.getTimestamp() - lastSwap >= period)
     {
       lastSwap = Timer.getTimestamp();
       evenCycle = !evenCycle;
