dc7e11a4a50e4ef2d987f734ed951ee1ac2af09b
Driver <coach@projectb.net>
2026-04-30 16:12:19 -0500

    Calibrating turret angle

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -279,9 +279,9 @@ public final class Constants
       /** Angle range of potentiometer giving output of [0..1], degrees */
       public static final double potRange = 3600;
       /** Angle offset to give 0 when turret is at centre, degrees */
-      public static final double portPotOffset = -1783.00;
+      public static final double portPotOffset = -1790.00;
       /** Angle offset to give 0 when turret is at centre, degrees */
-      public static final double stbdPotOffset = -1577.00;
+      public static final double stbdPotOffset = -1805.00;
 
       private static final double planetaryRatio = 13.03; // MaxPlanetary gearbox marked 4:1 is actually 3.6:1, 5:1 is actually 5.2:1
       private static final double driveGear = 15;
