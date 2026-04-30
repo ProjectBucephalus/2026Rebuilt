c8fc3e4ae8950c1d2545794a69ca581254f92661
Lego-archer-elf <Lego.archer.elf@gmail.com>
2026-05-01 03:54:27 +1000

    Adjusting ball paths for smoother transitions

--- src/main/java/frc/robot/constants/Path.java
+++ src/main/java/frc/robot/constants/Path.java
@@ -183,8 +183,8 @@ public record Path(double throttle, Node... nodes)
       (
         0.7,
         0.5, 
-        new Pose2d(7.25, 0.625, Rotation2d.kCCW_90deg),
-        new Pose2d(7.75, 1.25, Rotation2d.kCCW_90deg),
+        new Pose2d(7.25, 1, Rotation2d.kCCW_90deg),
+        new Pose2d(7.75, 1.5, Rotation2d.kCCW_90deg),
         new Pose2d(7.75, 3.5, Rotation2d.kCCW_90deg)
       )
     );
@@ -196,8 +196,8 @@ public record Path(double throttle, Node... nodes)
       (
         0.7,
         0.5, 
-        new Pose2d(7.25, 7.455, Rotation2d.kCW_90deg),
-        new Pose2d(7.75, 6.73, Rotation2d.kCW_90deg),
+        new Pose2d(7.25, 7.08, Rotation2d.kCW_90deg),
+        new Pose2d(7.75, 6.58, Rotation2d.kCW_90deg),
         new Pose2d(7.75, 4.58, Rotation2d.kCW_90deg)
       )
     );
@@ -238,8 +238,8 @@ public record Path(double throttle, Node... nodes)
       (
         0.7,
         0.5, 
-        new Pose2d(5.75, 0.625, Rotation2d.kCCW_90deg),
-        new Pose2d(6.25, 1.25, Rotation2d.kCCW_90deg),
+        new Pose2d(6.75, 1, Rotation2d.kCCW_90deg),
+        new Pose2d(6.75, 2.5, Rotation2d.kCCW_90deg),
         new Pose2d(6.25, 3.5, Rotation2d.kCCW_90deg)
       )
     );
@@ -251,9 +251,9 @@ public record Path(double throttle, Node... nodes)
       (
         0.7,
         0.5,
-        new Pose2d(6.25, 7.455, Rotation2d.kCW_90deg),
-        new Pose2d(6.75, 6.73, Rotation2d.kCW_90deg),
-        new Pose2d(6.75, 4.58, Rotation2d.kCW_90deg)
+        new Pose2d(6.75, 7.08, Rotation2d.kCW_90deg),
+        new Pose2d(6.75, 5.58, Rotation2d.kCW_90deg),
+        new Pose2d(6.25, 4.58, Rotation2d.kCW_90deg)
       )
     );
     // 11: Depo left to right
