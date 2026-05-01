f2125eb43a42ae6f73225e698ae1e9d21c2f7325
Lego-archer-elf <Lego.archer.elf@gmail.com>
2026-05-02 02:15:19 +1000

    Increasing turret current limit

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -321,7 +321,7 @@ public final class Constants
         turretConfig.MotionMagic.MotionMagicCruiseVelocity = turretTurnSpeed;
         turretConfig.MotionMagic.MotionMagicAcceleration = turretTurnSpeed * 5;
 
-        turretConfig.CurrentLimits.StatorCurrentLimit = 12;
+        turretConfig.CurrentLimits.StatorCurrentLimit = 30;
         turretConfig.CurrentLimits.StatorCurrentLimitEnable = true;
       }
     }
