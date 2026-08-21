291db4a9bcd8b5b7d864771bb3fad0a5397954d5
Siren_The_Pterafin <cheeky4pratt@gmail.com>
2026-08-21 11:43:53 +1000

    Display fence import

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -421,6 +421,79 @@ public class Robot extends TimedRobot
     );
   }
 
+   private void configureFenceBindings()
+  {
+    new Trigger(() -> PBDash.D_FENCE_SET.button()).and(() -> PBDash.STATE_DEMO.get()).onTrue(Commands.runOnce(() -> 
+    {
+      setFieldWall();
+    }));
+
+    new Trigger(() -> PBDash.D_FENCE_XAP.button()).and(() -> PBDash.STATE_DEMO.get())
+        .onTrue(Commands.runOnce(() -> 
+        {
+          FieldUtils.GeoFencing.field.contractBox(0.2, 0);
+          PBDash.D_FENCE_XA.put(PBDash.D_FENCE_XA.get() + 0.2);
+          renderFieldWall();
+        }));
+
+    new Trigger(() -> PBDash.D_FENCE_XAM.button()).and(() -> PBDash.STATE_DEMO.get())
+        .onTrue(Commands.runOnce(() -> 
+        {
+          FieldUtils.GeoFencing.field.expandBox(-0.2, 0);
+          PBDash.D_FENCE_XA.put(PBDash.D_FENCE_XA.get() - 0.2);
+          renderFieldWall();
+        }));
+
+    new Trigger(() -> PBDash.D_FENCE_YAP.button()).and(() -> PBDash.STATE_DEMO.get())
+        .onTrue(Commands.runOnce(() -> 
+        {
+          FieldUtils.GeoFencing.field.contractBox(0, 0.2);
+          PBDash.D_FENCE_YA.put(PBDash.D_FENCE_YA.get() + 0.2);
+          renderFieldWall();
+        }));
+
+    new Trigger(() -> PBDash.D_FENCE_YAM.button()).and(() -> PBDash.STATE_DEMO.get())
+        .onTrue(Commands.runOnce(() -> 
+        {
+          FieldUtils.GeoFencing.field.expandBox(0, -0.2);
+          PBDash.D_FENCE_YA.put(PBDash.D_FENCE_YA.get() - 0.2);
+          renderFieldWall();
+        }));
+
+    new Trigger(() -> PBDash.D_FENCE_XBP.button()).and(() -> PBDash.STATE_DEMO.get())
+        .onTrue(Commands.runOnce(() -> 
+        {
+          FieldUtils.GeoFencing.field.expandBox(0.2, 0);
+          PBDash.D_FENCE_XB.put(PBDash.D_FENCE_XB.get() + 0.2);
+          renderFieldWall();
+        }));
+
+    new Trigger(() -> PBDash.D_FENCE_XBM.button()).and(() -> PBDash.STATE_DEMO.get())
+        .onTrue(Commands.runOnce(() -> 
+        {
+          FieldUtils.GeoFencing.field.contractBox(-0.2, 0);
+          PBDash.D_FENCE_XB.put(PBDash.D_FENCE_XB.get() - 0.2);
+          renderFieldWall();
+        }));
+
+    new Trigger(() -> PBDash.D_FENCE_YBP.button()).and(() -> PBDash.STATE_DEMO.get())
+        .onTrue(Commands.runOnce(() -> 
+        {
+          FieldUtils.GeoFencing.field.expandBox(0, 0.2);
+          PBDash.D_FENCE_YB.put(PBDash.D_FENCE_YB.get() + 0.2);
+          renderFieldWall();
+        }));
+
+    new Trigger(() -> PBDash.D_FENCE_YBM.button()).and(() -> PBDash.STATE_DEMO.get())
+        .onTrue(Commands.runOnce(() -> 
+        {
+          FieldUtils.GeoFencing.field.contractBox(0, -0.2);
+          PBDash.D_FENCE_YB.put(PBDash.D_FENCE_YB.get() - 0.2);
+          renderFieldWall();
+        }));
+
+  }
+
   /* UTIL METHODS */
   /* ============ */
 
